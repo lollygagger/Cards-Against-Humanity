e8ee91d223d6df3de4debd0bc1fdc8fb63d8fba5
lollygagger <ubercharge60@gmail.com>
2022-02-04 16:40:12 -0500

    implemented retreiving players and displaying active players in  PlayerDisplay

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -1,5 +1,6 @@
 import {
-    useState
+    useState,
+    useEffect
 } from "react";
 import {
     Card,
@@ -11,9 +12,38 @@ import {
 
 export default function PlayerDisplay(props){
 
+    const [players, setPlayers] = useState([])
+
+    useEffect(() => {
+        fetch('/players', {method: 'GET'})
+            .then((response ) => response.json())
+            .then((json) => {
+                if(json != null) {
+                    console.log(json)
+                    setPlayers(json[0])
+                }else{
+                    setPlayers([props.username])
+                }
+            })
+            .catch((error) => console.log(error))
+
+    }, [])
+
+
+
+
     return(
         <div>
 
+            {players.map((player, index) => {
+                return(
+                    <div key={index}>
+                        <Col>
+                        <Card>{player}</Card>
+                        </Col>
+                    </div>
+                )
+            })}
         </div>
     )
 }
